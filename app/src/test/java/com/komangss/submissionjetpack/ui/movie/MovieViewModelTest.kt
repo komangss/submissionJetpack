f0f27f9ff1861c9575325de9705e0e855db5d041
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-09-10 10:00:49 +0800

    test MovieViewModel

--- app/src/test/java/com/komangss/submissionjetpack/ui/movie/MovieViewModelTest.kt
+++ app/src/test/java/com/komangss/submissionjetpack/ui/movie/MovieViewModelTest.kt
@@ -1,23 +1,55 @@
-//package com.komangss.submissionjetpack.ui.movie
-//
-//import org.junit.Test
-//
-//import org.junit.Assert.*
-//import org.junit.Before
-//
-//class MovieViewModelTest {
-//
-//    private lateinit var viewModel : MovieViewModel
-//
-//    @Before
-//    fun setUp() {
-//        viewModel = MovieViewModel()
-//    }
-//
-//    @Test
-//    fun getMovies() {
-//        val movieEntities = viewModel.getMovies()
-//        assertNotNull(movieEntities)
-//        assertEquals(16, movieEntities.size)
-//    }
-//}
+package com.komangss.submissionjetpack.ui.movie
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.komangss.submissionjetpack.data.CatalogRepository
+import com.komangss.submissionjetpack.data.source.local.entity.MovieEntity
+import com.komangss.submissionjetpack.utils.DataGenerator
+import com.nhaarman.mockitokotlin2.verify
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class MovieViewModelTest {
+
+    private lateinit var viewModel: MovieViewModel
+
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var catalogRepository: CatalogRepository
+
+    @Mock
+    private lateinit var observer: Observer<List<MovieEntity>>
+
+    @Before
+    fun setUp() {
+        viewModel = MovieViewModel(catalogRepository)
+    }
+
+    @Test
+    fun getMovies() {
+        val dummyMovies = DataGenerator.generateDummyMovieEntities()
+        val movies = MutableLiveData<List<MovieEntity>>()
+        movies.value = dummyMovies
+
+        `when`(catalogRepository.getAllMovies()).thenReturn(movies)
+        val movieEntities = viewModel.getMovies().value
+        verify(catalogRepository).getAllMovies()
+        assertNotNull(movieEntities)
+        assertEquals(16, movieEntities?.size)
+
+        viewModel.getMovies().observeForever(observer)
+        verify(observer).onChanged(dummyMovies)
+    }
+}
