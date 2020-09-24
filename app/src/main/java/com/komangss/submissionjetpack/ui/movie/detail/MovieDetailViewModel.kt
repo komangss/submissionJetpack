0a1f88986616603b8fa63926e3f2a51d4a6fc8ee
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-09-24 23:44:33 +0800

    inherit ViewModel in MovieDetailViewModel

--- app/src/main/java/com/komangss/submissionjetpack/ui/movie/detail/MovieDetailViewModel.kt
+++ app/src/main/java/com/komangss/submissionjetpack/ui/movie/detail/MovieDetailViewModel.kt
@@ -1,10 +1,11 @@
 package com.komangss.submissionjetpack.ui.movie.detail
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
 import com.komangss.submissionjetpack.data.CatalogRepository
 import com.komangss.submissionjetpack.data.source.local.entity.MovieEntity
 
-class MovieDetailViewModel (private val catalogRepository: CatalogRepository) {
+class MovieDetailViewModel (private val catalogRepository: CatalogRepository) : ViewModel() {
     fun detailMovie(id : Int) : LiveData<MovieEntity> {
         return catalogRepository.getMovieById(id)
     }
