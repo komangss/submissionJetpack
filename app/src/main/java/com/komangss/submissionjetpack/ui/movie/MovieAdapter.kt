32d194320b649442b332ab8f671c336b40f84d7d
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-09-24 23:40:17 +0800

    add click listener for each item in movie list

--- app/src/main/java/com/komangss/submissionjetpack/ui/movie/MovieAdapter.kt
+++ app/src/main/java/com/komangss/submissionjetpack/ui/movie/MovieAdapter.kt
@@ -1,5 +1,6 @@
 package com.komangss.submissionjetpack.ui.movie
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.komangss.submissionjetpack.R
 import com.komangss.submissionjetpack.data.source.local.entity.MovieEntity
+import com.komangss.submissionjetpack.ui.movie.detail.MovieDetailActivity
 import kotlinx.android.synthetic.main.items_movie_and_tvshow.view.*
 
 class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
@@ -38,13 +40,15 @@ class MovieAdapter : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
                 item_movie_tvshow_tv_item_title.text = movie.title
                 item_movie_tvshow_tv_description.text = movie.description
 
-
                 Glide.with(context)
                     .load(resources.getDrawable(movie.image))
                     .into(item_movie_tvshow_image_view_poster)
+
+                setOnClickListener {
+                    context.startActivity(Intent(context, MovieDetailActivity::class.java))
+                }
             }
 
-//            TODO : Create Detail Movie
         }
     }
 }
