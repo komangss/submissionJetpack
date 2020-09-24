fbe19904c471598e2e1632c70fc6930a656d3000
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-09-25 01:08:51 +0800

    make response and entity data generator separately

--- app/src/main/java/com/komangss/submissionjetpack/ui/movie/MovieViewModel.kt
+++ app/src/main/java/com/komangss/submissionjetpack/ui/movie/MovieViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.komangss.submissionjetpack.data.CatalogRepository
 import com.komangss.submissionjetpack.data.source.local.entity.MovieEntity
-import com.komangss.submissionjetpack.utils.DataGenerator
 
 class MovieViewModel(private val catalogRepository: CatalogRepository) : ViewModel() {
     fun getMovies() : LiveData<List<MovieEntity>> =
