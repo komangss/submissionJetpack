9538b833aefdda88151f116696c8228f92b3f693
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-09-27 20:13:53 +0800

    add component in movie detail

--- app/src/main/java/com/komangss/submissionjetpack/ui/movie/detail/MovieDetailActivity.kt
+++ app/src/main/java/com/komangss/submissionjetpack/ui/movie/detail/MovieDetailActivity.kt
@@ -2,11 +2,12 @@ package com.komangss.submissionjetpack.ui.movie.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
 import com.komangss.submissionjetpack.R
 import com.komangss.submissionjetpack.viewmodel.ViewModelFactory
+import kotlinx.android.synthetic.main.activity_movie_detail.*
 
 class MovieDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +21,12 @@ class MovieDetailActivity : AppCompatActivity() {
             ViewModelProvider(this, factory)[MovieDetailViewModel::class.java]
 
         viewModel.detailMovie(movieId).observe(this, Observer {
-            Toast.makeText(this, it.title, Toast.LENGTH_LONG).show()
+            tv_activity_movie_detail_movie_title.text = it.title
+            tv_activity_movie_detail_movie_description.text = it.description
+            tv_activity_movie_detail_movie_rating_movie.text = it.rating
+            Glide.with(this@MovieDetailActivity)
+                .load(resources.getDrawable(it.image))
+                .into(image_view_activity_movie_detail_movie_poster)
         })
     }
 
