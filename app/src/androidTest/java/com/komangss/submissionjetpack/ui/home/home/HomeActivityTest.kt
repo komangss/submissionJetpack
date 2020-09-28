b6101f8e6a7d11a2668e6be4afeec105a8af510b
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-09-28 15:58:02 +0800

    Fix deprecated ActivityTestRule with new ActivityScenarioRule in HomeActivityTest

--- app/src/androidTest/java/com/komangss/submissionjetpack/ui/home/home/HomeActivityTest.kt
+++ app/src/androidTest/java/com/komangss/submissionjetpack/ui/home/home/HomeActivityTest.kt
@@ -1,33 +1,20 @@
 package com.komangss.submissionjetpack.ui.home.home
 
-import android.content.Intent
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.swipeLeft
 import androidx.test.espresso.action.ViewActions.swipeRight
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
-import androidx.test.rule.ActivityTestRule
 import com.komangss.submissionjetpack.R
 import com.komangss.submissionjetpack.ui.home.HomeActivity
-import org.hamcrest.core.IsNull.notNullValue
-import org.junit.Before
+import com.komangss.submissionjetpack.ui.home.rule.lazyActivityScenarioRule
 import org.junit.Rule
 import org.junit.Test
 
 class HomeActivityTest {
 
     @get:Rule
-    val activityTestRule : ActivityTestRule<HomeActivity> =
-        ActivityTestRule(HomeActivity::class.java)
-
-    private lateinit var activity : HomeActivity
-
-    @Before
-    fun setUp() {
-        activity = activityTestRule.activity
-        activityTestRule.launchActivity(Intent())
-        assertThat(activityTestRule, notNullValue())
-    }
+    val rule = lazyActivityScenarioRule<HomeActivity>(launchActivity = true)
 
     @Test
     fun swipePage() {
