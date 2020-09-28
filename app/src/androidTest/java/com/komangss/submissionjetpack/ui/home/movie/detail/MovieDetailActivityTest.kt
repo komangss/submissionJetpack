48532be0773906d78ee9315e80312ed38b0b071d
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-09-28 14:25:04 +0800

    Create UI Test for movie detail activity with espresso idling resources

--- app/src/androidTest/java/com/komangss/submissionjetpack/ui/home/movie/detail/MovieDetailActivityTest.kt
+++ app/src/androidTest/java/com/komangss/submissionjetpack/ui/home/movie/detail/MovieDetailActivityTest.kt
@@ -1,11 +1,21 @@
 package com.komangss.submissionjetpack.ui.home.movie.detail
 
 import android.content.Intent
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.rule.ActivityTestRule
+import com.komangss.submissionjetpack.R
 import com.komangss.submissionjetpack.ui.movie.detail.MovieDetailActivity
+import com.komangss.submissionjetpack.ui.movie.detail.MovieDetailActivity.Companion.EXTRA_MOVIE_ID
 import com.komangss.submissionjetpack.utils.DomainModelDataGenerator
+import com.komangss.submissionjetpack.utils.EspressoIdlingResources
+import org.junit.After
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 class MovieDetailActivityTest {
 
@@ -21,8 +31,20 @@ class MovieDetailActivityTest {
 
     @Before
     fun setUp() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResources.countingIdlingResource)
         val intent = Intent()
-        intent.putExtra("EXTRA_MOVIE_ID", dummyMovie.id)
+        intent.putExtra(EXTRA_MOVIE_ID, dummyMovie.id)
         activityTestRule.launchActivity(intent)
+    }
+
+    @Test
+    fun testMovieTitleShowUp() {
+        Espresso.onView(ViewMatchers.withId(R.id.tv_activity_movie_detail_movie_title))
+            .check(ViewAssertions.matches(withText(dummyMovie.title)))
+    }
+
+    @After
+    fun unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResources.countingIdlingResource)
     }
 }
