a3bfd8fc9bea64a78a29b48c8367dfabedb9224b
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-09-30 17:33:02 +0800

    add some method to dao

--- app/src/main/java/com/komangss/submissionjetpack/framework/cache/dao/CatalogDao.kt
+++ app/src/main/java/com/komangss/submissionjetpack/framework/cache/dao/CatalogDao.kt
@@ -2,6 +2,9 @@ package com.komangss.submissionjetpack.framework.cache.dao
 
 import androidx.annotation.WorkerThread
 import androidx.lifecycle.LiveData
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.komangss.submissionjetpack.framework.cache.model.MovieEntity
 
@@ -10,4 +13,10 @@ interface CatalogDao {
     @WorkerThread
     @Query("SELECT * FROM movie_entity")
     fun getMovies(): LiveData<List<MovieEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertMovies(movies: List<MovieEntity>): LongArray
+
+    @Delete
+    fun deleteMovie(movie: MovieEntity): Int
 }
