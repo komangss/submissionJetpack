9eadcf1e5865d2b631d38512e3e1721ae52e0bbc
Antonio Komang Yudistira <komanggt12@gmail.com>
2020-11-14 01:00:45 +0800

    change movie model structure to fit tmdb movies result

--- app/src/main/java/com/komangss/submissionjetpack/framework/network/model/MovieResponse.kt
+++ app/src/main/java/com/komangss/submissionjetpack/framework/network/model/MovieResponse.kt
@@ -1,17 +1,51 @@
 package com.komangss.submissionjetpack.framework.network.model
 
 import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class MovieResponse(
-    val id : Int,
-    val title : String,
-    val director: String,
-    val description : String,
-    val image : String,
-    val releaseDate : String,
-    val rating : String
+    @SerializedName("popularity")
+    val popularity: Double,
+    @SerializedName("vote_count")
+    val voteCount: Int,
+    @SerializedName("video")
+    val isVideo: Boolean,
+    @SerializedName("poster_path")
+    val posterUrlPath: String,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("backdrop_path")
+    val backdropUrlPath: String,
+    @SerializedName("original_language")
+    val originalLanguage: String,
+    @SerializedName("original_title")
+    val originalTitle: String,
+    @SerializedName("genre_ids")
+    val genreIds: List<Int>?, // TODO : Research how to create and find the genre with the id using custom class
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("vote_average")
+    val voteAverage: Double,
+    @SerializedName("overview")
+    val description: String,
+    @SerializedName("release_date")
+    val releaseDate: String
 ) : Parcelable {
-    constructor() : this(0, "", "", "", "", "","")
+    constructor() : this (
+        0.0,
+        0,
+        false,
+        "",
+        0,
+        "",
+        "",
+        "",
+        null,
+        "",
+    0.0,
+    "",
+    ""
+    )
 }
