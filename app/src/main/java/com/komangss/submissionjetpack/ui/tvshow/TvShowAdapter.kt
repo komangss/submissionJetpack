d67763fa13238c92ae39cf42caba833838804a22
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-01-05 03:10:22 +0800

    fix ui in tv show fragment

--- app/src/main/java/com/komangss/submissionjetpack/ui/tvshow/TvShowAdapter.kt
+++ app/src/main/java/com/komangss/submissionjetpack/ui/tvshow/TvShowAdapter.kt
@@ -5,15 +5,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.komangss.submissionjetpack.BuildConfig
 import com.komangss.submissionjetpack.R
-import com.komangss.submissionjetpack.framework.cache.model.TvShowEntity
+import com.komangss.submissionjetpack.business.domain.model.TvShow
 import kotlinx.android.synthetic.main.items_movie_and_tvshow.view.*
 
 class TvShowAdapter : RecyclerView.Adapter<TvShowAdapter.TvShowViewHolder>() {
 
-    private val tvShowList = ArrayList<TvShowEntity>()
+    private val tvShowList = ArrayList<TvShow>()
 
-    fun setTvShows(tvShowList : List<TvShowEntity>) {
+    fun setTvShows(tvShowList : List<TvShow>) {
         this.tvShowList.clear()
         this.tvShowList.addAll(tvShowList)
         notifyDataSetChanged()
@@ -33,14 +34,15 @@ class TvShowAdapter : RecyclerView.Adapter<TvShowAdapter.TvShowViewHolder>() {
     }
 
     class TvShowViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(tvShow : TvShowEntity) {
+        fun bind(tvShow : TvShow) {
             with(itemView) {
                 item_movie_tvshow_tv_item_title.text = tvShow.title
                 item_movie_tvshow_tv_description.text = tvShow.description
 
                 Glide.with(context)
-                    .load(tvShow.image)
+                    .load(resources.getIdentifier(tvShow.image, "drawable", BuildConfig.APPLICATION_ID))
                     .into(item_movie_tvshow_image_view_poster)
+
             }
 
 //            TODO : Create Detail TvShow
--- app/src/main/java/com/komangss/submissionjetpack/ui/tvshow/TvShowFragment.kt
+++ app/src/main/java/com/komangss/submissionjetpack/ui/tvshow/TvShowFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.komangss.submissionjetpack.R
+import com.komangss.submissionjetpack.ui.movie.MovieViewModel
+import com.komangss.submissionjetpack.viewmodel.ViewModelFactory
 import kotlinx.android.synthetic.main.fragment_tv_show.*
 
 
@@ -27,11 +30,15 @@ class TvShowFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         if (activity != null) {
 
-//            val viewModel = ViewModelProvider(this).get(TvShowViewModel::class.java)
-//            val tvShows = viewModel.g()
+            val factory = ViewModelFactory.getInstance(requireActivity())
+            val viewModel = ViewModelProvider(this, factory)[TvShowViewModel::class.java]
 
             val tvShowAdapter = TvShowAdapter()
-//            tvShowAdapter.setTvShows(tvShows)
+
+            viewModel.getTvShows().observe(viewLifecycleOwner, {
+                tvShowAdapter.setTvShows(it)
+                tvShowAdapter.notifyDataSetChanged()
+            })
 
             with(fragment_tvshow_rv_tvshow) {
                 layoutManager = LinearLayoutManager(context)
