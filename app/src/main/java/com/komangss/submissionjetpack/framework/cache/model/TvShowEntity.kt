5058ded0d6dac93987b405a9162fe5dce9abccba
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-01-06 11:11:06 +0800

    create entity model for tv show

---
+++ app/src/main/java/com/komangss/submissionjetpack/framework/cache/model/TvShowEntity.kt
@@ -0,0 +1,22 @@
+package com.komangss.submissionjetpack.framework.cache.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName= "tv_show_entity")
+data class TvShowEntity (
+    val backdropUrlPath: String,
+    val releaseDate: String,
+    val genreIds: String,
+    @PrimaryKey(autoGenerate = false)
+    val id: Int,
+    val name: String,
+    val originalCountry : String,
+    val originalLanguage: String,
+    val originalName: String,
+    val description: String,
+    val popularity: Double,
+    val posterUrlPath: String,
+    val voteAverage: Double,
+    val voteCount: Int
+)
