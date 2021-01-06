9c9bb91d24ea07adcd12e77782f60a3924a7d409
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-01-06 11:03:38 +0800

    create tv show network model

--- app/src/main/java/com/komangss/submissionjetpack/framework/network/model/TvShowResponse.kt
+++ app/src/main/java/com/komangss/submissionjetpack/framework/network/model/TvShowResponse.kt
@@ -1,10 +1,32 @@
 package com.komangss.submissionjetpack.framework.network.model
 
+import com.google.gson.annotations.SerializedName
+
 data class TvShowResponse (
-    val id : Int,
-    val title : String,
-    val description : String,
-    val image : String,
-    val releaseDate : String,
-    val rating : String
+    @SerializedName("backdrop_path")
+    val backdropUrlPath: String,
+    @SerializedName("first_air_date")
+    val releaseDate: String,
+    @SerializedName("genre_ids")
+    val genreIds: List<Int>?,
+    @SerializedName("id")
+    val id: Int,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("origin_country")
+    val originalCountry : List<String>?,
+    @SerializedName("original_language")
+    val originalLanguage: String,
+    @SerializedName("original_name")
+    val originalName: String,
+    @SerializedName("overview")
+    val description: String,
+    @SerializedName("popularity")
+    val popularity: Double,
+    @SerializedName("poster_path")
+    val posterUrlPath: String,
+    @SerializedName("vote_average")
+    val voteAverage: Double,
+    @SerializedName("vote_count")
+    val voteCount: Int
 )
