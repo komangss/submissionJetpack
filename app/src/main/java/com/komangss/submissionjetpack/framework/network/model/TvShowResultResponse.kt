7e8971727503942eaafc62a0c2625134dadb933e
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-01-07 23:30:37 +0800

    create tv show result response model

---
+++ app/src/main/java/com/komangss/submissionjetpack/framework/network/model/TvShowResultResponse.kt
@@ -0,0 +1,14 @@
+package com.komangss.submissionjetpack.framework.network.model
+
+import com.google.gson.annotations.SerializedName
+
+data class TvShowResultResponse (
+    @SerializedName("page")
+    val page : Int,
+    @SerializedName("total_results")
+    val totalResults : Int,
+    @SerializedName("total_pages")
+    val totalPages : Int,
+    @SerializedName("results")
+    val results : List<TvShowResponse>
+)
