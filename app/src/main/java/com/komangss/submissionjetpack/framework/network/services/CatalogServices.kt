46bc706d67a6fbdce22fc2f1bf98c0c2fcf77b9f
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-01-07 23:35:45 +0800

    add tv show get service

--- app/src/main/java/com/komangss/submissionjetpack/framework/network/services/CatalogServices.kt
+++ app/src/main/java/com/komangss/submissionjetpack/framework/network/services/CatalogServices.kt
@@ -2,9 +2,13 @@ package com.komangss.submissionjetpack.framework.network.services
 
 import com.komangss.submissionjetpack.BuildConfig
 import com.komangss.submissionjetpack.framework.network.model.MovieResultResponse
+import com.komangss.submissionjetpack.framework.network.model.TvShowResultResponse
 import retrofit2.http.GET
 
 interface CatalogServices {
     @GET("discover/movie?api_key=${BuildConfig.TMDB_API_KEY}")
     suspend fun getMovies() : MovieResultResponse
+
+    @GET("tv/popular?api_key=${BuildConfig.TMDB_API_KEY}")
+    suspend fun getTvShows() : TvShowResultResponse
 }
