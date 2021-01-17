1938e87f1b4185d15b40fd2569d68264d17b2fb8
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-01-18 04:11:28 +0800

    create movieDetail view model

--- app/src/main/java/com/komangss/submissionjetpack/ui/movie/detail/MovieDetailViewModel.kt
+++ app/src/main/java/com/komangss/submissionjetpack/ui/movie/detail/MovieDetailViewModel.kt
@@ -1,12 +1,18 @@
-//package com.komangss.submissionjetpack.ui.movie.detail
-//
-//import androidx.lifecycle.LiveData
-//import androidx.lifecycle.ViewModel
-//import com.komangss.submissionjetpack.business.domain.model.Movie
-//import com.komangss.submissionjetpack.business.repository.CatalogRepository
-//
-//class MovieDetailViewModel (private val catalogRepository: CatalogRepository) : ViewModel() {
-//    fun detailMovie(id : Int) : LiveData<Movie> {
-//        return catalogRepository.getMovieById(id)
-//    }
-//}
+package com.komangss.submissionjetpack.ui.movie.detail
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.liveData
+import com.komangss.submissionjetpack.business.domain.model.MovieDetail
+import com.komangss.submissionjetpack.business.repository.CatalogRepository
+import com.komangss.submissionjetpack.vo.Resource
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+
+class MovieDetailViewModel (private val catalogRepository: CatalogRepository) : ViewModel() {
+    @ExperimentalCoroutinesApi
+    fun detailMovie(id : Int) : LiveData<Resource<MovieDetail>> = liveData {
+        emit(Resource.InProgress)
+        emitSource(catalogRepository.getMovieById(id).asLiveData())
+    }
+}
