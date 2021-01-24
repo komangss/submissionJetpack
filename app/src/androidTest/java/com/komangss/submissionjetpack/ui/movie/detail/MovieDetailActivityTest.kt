53542e44ea2445e8a8569541541697a505a8ddf2
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-01-25 07:02:44 +0800

    integration and ui test for all layout

--- app/src/androidTest/java/com/komangss/submissionjetpack/ui/movie/detail/MovieDetailActivityTest.kt
+++ app/src/androidTest/java/com/komangss/submissionjetpack/ui/movie/detail/MovieDetailActivityTest.kt
@@ -1,49 +1,71 @@
-//package com.komangss.submissionjetpack.ui.movie.detail
-//
-//import android.content.Intent
-//import androidx.test.core.app.ApplicationProvider
-//import androidx.test.espresso.Espresso
-//import androidx.test.espresso.IdlingRegistry
-//import androidx.test.espresso.assertion.ViewAssertions
-//import androidx.test.espresso.matcher.ViewMatchers
-//import androidx.test.espresso.matcher.ViewMatchers.withText
-//import com.komangss.submissionjetpack.R
-//import com.komangss.submissionjetpack.ui.rule.lazyActivityScenarioRule
-//import com.komangss.submissionjetpack.ui.movie.detail.MovieDetailActivity.Companion.EXTRA_MOVIE_ID
-//import com.komangss.submissionjetpack.utils.DomainModelDataGenerator
-//import com.komangss.submissionjetpack.utils.EspressoIdlingResources
-//import org.junit.After
-//import org.junit.Before
-//import org.junit.Rule
-//import org.junit.Test
-//
-//class MovieDetailActivityTest {
-//
-//    private val dummyMovie = DomainModelDataGenerator.generateDummyMovies()[0]
-//
-//    @get:Rule
-//    val rule = lazyActivityScenarioRule<MovieDetailActivity>(launchActivity = false)
-//
-//    @Before
-//    fun setUp() {
-//        IdlingRegistry.getInstance().register(EspressoIdlingResources.countingIdlingResource)
-//    }
-//
-//    @Test
-//    fun testMovieTitleShowUp() {
-//        val intent = Intent(
-//            ApplicationProvider.getApplicationContext(), MovieDetailActivity::class.java
-//        ).putExtra(EXTRA_MOVIE_ID, dummyMovie.id)
-//
-//        rule.launch(intent)
-//
-//        Espresso.onView(ViewMatchers.withId(R.id.tv_activity_movie_detail_movie_title))
-//            .check(ViewAssertions.matches(withText(dummyMovie.title)))
-//
-//    }
-//
-//    @After
-//    fun unregisterIdlingResource() {
-//        IdlingRegistry.getInstance().unregister(EspressoIdlingResources.countingIdlingResource)
-//    }
-//}
+package com.komangss.submissionjetpack.ui.movie.detail
+
+import android.content.Intent
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.platform.app.InstrumentationRegistry
+import com.google.gson.GsonBuilder
+import com.komangss.submissionjetpack.R
+import com.komangss.submissionjetpack.business.domain.model.Movie
+import com.komangss.submissionjetpack.framework.mapper.CatalogMovieMapper
+import com.komangss.submissionjetpack.framework.network.model.MovieResultResponse
+import com.komangss.submissionjetpack.ui.Utils.getJsonFromAssets
+import com.komangss.submissionjetpack.ui.movie.detail.MovieDetailActivity.Companion.EXTRA_MOVIE_ID
+import com.komangss.submissionjetpack.ui.rule.lazyActivityScenarioRule
+import com.komangss.submissionjetpack.utils.EspressoIdlingResources
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class MovieDetailActivityTest {
+
+    private lateinit var dummyMovie: Movie
+
+    @get:Rule
+    val rule = lazyActivityScenarioRule<MovieDetailActivity>(launchActivity = false)
+
+    @Before
+    fun setUp() {
+        val gson = GsonBuilder().create()
+        val jsonResult =
+            getJsonFromAssets(
+                InstrumentationRegistry.getInstrumentation().context,
+                "MovieResponse.json"
+            )
+
+        val movieResultResponse: MovieResultResponse = gson.fromJson(
+            jsonResult,
+            MovieResultResponse::class.java
+        )
+
+        val mapper = CatalogMovieMapper()
+        val entities = mapper.responsesToEntities(movieResultResponse.results)
+
+        dummyMovie = mapper.entitiesToDomains(entities)[0]
+
+        IdlingRegistry.getInstance().register(EspressoIdlingResources.countingIdlingResource)
+    }
+
+    @Test
+    fun testMovieTitleShowUp() {
+        val intent = Intent(
+            ApplicationProvider.getApplicationContext(), MovieDetailActivity::class.java
+        ).putExtra(EXTRA_MOVIE_ID, dummyMovie.id)
+
+        rule.launch(intent)
+
+        Espresso.onView(ViewMatchers.withId(R.id.tv_activity_movie_detail_movie_title))
+            .check(ViewAssertions.matches(withText(dummyMovie.title)))
+
+    }
+
+    @After
+    fun unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResources.countingIdlingResource)
+    }
+}
--- app/src/androidTest/java/com/komangss/submissionjetpack/ui/tvshow/detail/TvShowDetailActivityTest.kt
+++ app/src/androidTest/java/com/komangss/submissionjetpack/ui/tvshow/detail/TvShowDetailActivityTest.kt
@@ -6,24 +6,47 @@ import androidx.test.espresso.Espresso
 import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.platform.app.InstrumentationRegistry
+import com.google.gson.GsonBuilder
 import com.komangss.submissionjetpack.R
+import com.komangss.submissionjetpack.business.domain.model.TvShow
+import com.komangss.submissionjetpack.framework.mapper.CatalogTvShowMapper
+import com.komangss.submissionjetpack.framework.network.model.TvShowResultResponse
+import com.komangss.submissionjetpack.ui.Utils
 import com.komangss.submissionjetpack.ui.rule.lazyActivityScenarioRule
 import com.komangss.submissionjetpack.ui.tvshow.detail.TvShowDetailActivity.Companion.EXTRA_TV_SHOW_ID
-import com.komangss.submissionjetpack.utils.DomainModelDataGenerator
 import com.komangss.submissionjetpack.utils.EspressoIdlingResources
+import kotlinx.android.synthetic.main.activity_tv_show_detail.*
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 class TvShowDetailActivityTest {
-    private val dummyTvShow = DomainModelDataGenerator.generateDummyTvShows()[0]
+    private lateinit var dummyTvShow : TvShow
 
     @get:Rule
     val rule = lazyActivityScenarioRule<TvShowDetailActivity>(launchActivity = false)
 
     @Before
     fun setUp() {
+        val gson = GsonBuilder().create()
+        val jsonResult =
+            Utils.getJsonFromAssets(
+                InstrumentationRegistry.getInstrumentation().context,
+                "TvShowResponse.json"
+            )
+
+        val tvShowResultResponse: TvShowResultResponse = gson.fromJson(
+            jsonResult,
+            TvShowResultResponse::class.java
+        )
+
+        val mapper = CatalogTvShowMapper()
+        val entities = mapper.responsesToEntities(tvShowResultResponse.results)
+
+        dummyTvShow = mapper.entitiesToDomains(entities)[0]
+
         IdlingRegistry.getInstance().register(EspressoIdlingResources.countingIdlingResource)
     }
 
@@ -36,7 +59,7 @@ class TvShowDetailActivityTest {
         rule.launch(intent)
 
         Espresso.onView(ViewMatchers.withId(R.id.tv_activity_tv_show_detail_tv_show_title))
-            .check(ViewAssertions.matches(ViewMatchers.withText(dummyTvShow.title)))
+            .check(ViewAssertions.matches(ViewMatchers.withText(dummyTvShow.name)))
     }
 
     @After
