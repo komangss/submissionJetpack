f1080a279fbfdb980db945c2e83b57ad31e7f07e
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-01-25 06:49:48 +0800

    add utility to read json files for testing

---
+++ app/src/androidTest/java/com/komangss/submissionjetpack/ui/Utils.kt
@@ -0,0 +1,23 @@
+package com.komangss.submissionjetpack.ui
+
+import android.content.Context
+import java.io.IOException
+import java.io.InputStream
+
+object Utils {
+    fun getJsonFromAssets(context: Context, fileName: String): String? {
+        val jsonString: String
+        jsonString = try {
+            val `is`: InputStream = context.getAssets().open(fileName)
+            val size: Int = `is`.available()
+            val buffer = ByteArray(size)
+            `is`.read(buffer)
+            `is`.close()
+            String(buffer)
+        } catch (e: IOException) {
+            e.printStackTrace()
+            return null
+        }
+        return jsonString
+    }
+}
