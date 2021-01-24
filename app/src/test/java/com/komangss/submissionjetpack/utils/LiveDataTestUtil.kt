b6d15bb337506cfd125b5583b1c34e0a559b9184
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-01-25 06:59:56 +0800

    add new utility for next unit testing

--- app/src/test/java/com/komangss/submissionjetpack/utils/LiveDataTestUtil.kt
+++ app/src/test/java/com/komangss/submissionjetpack/utils/LiveDataTestUtil.kt
@@ -1,9 +1,12 @@
+@file:Suppress("UNCHECKED_CAST")
+
 package com.komangss.submissionjetpack.utils
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeoutException
 
 object LiveDataTestUtil {
     fun <T> getValue(liveData: LiveData<T>): T {
@@ -27,6 +30,33 @@ object LiveDataTestUtil {
         }
 
         return data[0] as T
+    }
 
+
+    /* Copyright 2019 Google LLC.
+       SPDX-License-Identifier: Apache-2.0 */
+    fun <T> LiveData<T>.getOrAwaitValue(
+        time: Long = 2,
+        timeUnit: TimeUnit = TimeUnit.SECONDS
+    ): T {
+        var data: T? = null
+        val latch = CountDownLatch(1)
+        val observer = object : Observer<T> {
+            override fun onChanged(o: T?) {
+                data = o
+                latch.countDown()
+                this@getOrAwaitValue.removeObserver(this)
+            }
+        }
+
+        this.observeForever(observer)
+
+        // Don't wait indefinitely if the LiveData is not set.
+        if (!latch.await(time, timeUnit)) {
+            throw TimeoutException("LiveData value was never set.")
+        }
+
+        @Suppress("UNCHECKED_CAST")
+        return data as T
     }
 }
--- app/src/test/java/com/komangss/submissionjetpack/utils/datagenerator/MovieDataGenerator.kt
+++ app/src/test/java/com/komangss/submissionjetpack/utils/datagenerator/MovieDataGenerator.kt
@@ -5,27 +5,29 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
 class MovieDataGenerator {
-    val dummyMovieEntities : Flow<List<MovieEntity>> = flow {
+    val dummyMovieEntities: Flow<List<MovieEntity>> = flow {
         emit(provideDummyMovieList())
     }
 
-    fun provideDummyMovieList() : ArrayList<MovieEntity> {
-        val dummyMovieList : ArrayList<MovieEntity> = ArrayList()
-        dummyMovieList.add(MovieEntity(
-            10.0,
-            5000,
-            false,
-            "http://example.com",
-            123,
-            "http://example.com",
-            "en-us",
-            "dummy title",
-            "[1, 2, 3]",
-            "dummy title",
-            10.0,
-            "dummy description",
-            "10-10-2020"
-        ))
+    fun provideDummyMovieList(): ArrayList<MovieEntity> {
+        val dummyMovieList: ArrayList<MovieEntity> = ArrayList()
+        dummyMovieList.add(
+            MovieEntity(
+                popularity = 2942.16,
+                voteCount = 2853,
+                isVideo = false,
+                posterUrlPath = "/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg",
+                id = 464052,
+                backdropUrlPath = "/srYya1ZlI97Au4jUYAktDe3avyA.jpg",
+                originalLanguage = "en",
+                originalTitle = "Wonder Woman 1984",
+                genreIds = "[14, 28, 12]",
+                title = "Wonder Woman 1984",
+                voteAverage = 7.1,
+                description = "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s and finds a formidable foe by the name of the Cheetah.",
+                releaseDate = "2020-12-16"
+            )
+        )
         return dummyMovieList
     }
 }
