e695489c432148c6ed71e8d6df2454819ad82d1d
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-01-25 06:56:58 +0800

    add get tv show by id in network services

--- app/src/main/java/com/komangss/submissionjetpack/framework/network/services/CatalogServices.kt
+++ app/src/main/java/com/komangss/submissionjetpack/framework/network/services/CatalogServices.kt
@@ -3,6 +3,7 @@ package com.komangss.submissionjetpack.framework.network.services
 import com.komangss.submissionjetpack.BuildConfig
 import com.komangss.submissionjetpack.framework.network.model.MovieDetailResponse
 import com.komangss.submissionjetpack.framework.network.model.MovieResultResponse
+import com.komangss.submissionjetpack.framework.network.model.TvShowDetailResponse
 import com.komangss.submissionjetpack.framework.network.model.TvShowResultResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -16,4 +17,7 @@ interface CatalogServices {
 
     @GET("movie/{id}?api_key=${BuildConfig.TMDB_API_KEY}")
     suspend fun getMovieById(@Path("id") id : Int) : MovieDetailResponse
+
+    @GET("tv/{tvShowId}?api_key=${BuildConfig.TMDB_API_KEY}")
+    suspend fun getTvShowById(@Path("tvShowId") id : Int) : TvShowDetailResponse
 }
