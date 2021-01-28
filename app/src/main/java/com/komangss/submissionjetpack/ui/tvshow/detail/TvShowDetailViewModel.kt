197fc42824444721da5b06463136899d5cd36ffc
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-01-29 04:45:12 +0800

    fix error in tv show detail view model

--- app/src/main/java/com/komangss/submissionjetpack/ui/tvshow/detail/TvShowDetailViewModel.kt
+++ app/src/main/java/com/komangss/submissionjetpack/ui/tvshow/detail/TvShowDetailViewModel.kt
@@ -4,14 +4,14 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.liveData
-import com.komangss.submissionjetpack.business.domain.model.TvShowDetail
+import com.komangss.submissionjetpack.business.domain.model.TvShow
 import com.komangss.submissionjetpack.business.repository.CatalogRepository
 import com.komangss.submissionjetpack.vo.Resource
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 class TvShowDetailViewModel(private val catalogRepository: CatalogRepository) : ViewModel() {
     @ExperimentalCoroutinesApi
-    fun detailTvShow(id : Int) : LiveData<Resource<TvShowDetail>> = liveData {
+    fun detailTvShow(id : Int) : LiveData<Resource<TvShow>> = liveData {
         emitSource(catalogRepository.getTvShowById(id).asLiveData())
     }
 }
