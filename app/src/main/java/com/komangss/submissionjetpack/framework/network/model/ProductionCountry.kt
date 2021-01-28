003c53a11b85bf1afda6382b1c2313b6f0e1cb7f
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-01-29 04:24:17 +0800

    remove later unused model

--- app/src/main/java/com/komangss/submissionjetpack/framework/network/model/ProductionCountry.kt
+++
@@ -1,9 +0,0 @@
-package com.komangss.submissionjetpack.framework.network.model
-
-import com.google.gson.annotations.SerializedName
-
-data class ProductionCountry(
-    @SerializedName("iso_3166_1")
-    val countryCode : String,
-    val name : String
-)
