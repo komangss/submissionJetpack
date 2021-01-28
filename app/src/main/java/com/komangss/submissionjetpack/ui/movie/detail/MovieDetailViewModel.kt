f166b83d7718c0be455a32270c3e8f83bfc7803d
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-01-29 04:43:17 +0800

    fix error in movie detail viewmodel

--- app/src/main/java/com/komangss/submissionjetpack/ui/movie/detail/MovieDetailViewModel.kt
+++ app/src/main/java/com/komangss/submissionjetpack/ui/movie/detail/MovieDetailViewModel.kt
@@ -4,14 +4,14 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.liveData
-import com.komangss.submissionjetpack.business.domain.model.MovieDetail
+import com.komangss.submissionjetpack.business.domain.model.Movie
 import com.komangss.submissionjetpack.business.repository.CatalogRepository
 import com.komangss.submissionjetpack.vo.Resource
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 class MovieDetailViewModel (private val catalogRepository: CatalogRepository) : ViewModel() {
     @ExperimentalCoroutinesApi
-    fun detailMovie(id : Int) : LiveData<Resource<MovieDetail>> = liveData {
+    fun detailMovie(id : Int) : LiveData<Resource<Movie>> = liveData {
         emitSource(catalogRepository.getMovieById(id).asLiveData())
     }
 }
