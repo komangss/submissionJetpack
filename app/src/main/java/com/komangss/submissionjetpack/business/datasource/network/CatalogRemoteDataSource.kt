3ca875032e6b51dd5993e5f54a10103da57e11b9
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-01-29 04:25:55 +0800

    remove later unused method in remote data source

--- app/src/main/java/com/komangss/submissionjetpack/business/datasource/network/CatalogRemoteDataSource.kt
+++ app/src/main/java/com/komangss/submissionjetpack/business/datasource/network/CatalogRemoteDataSource.kt
@@ -1,8 +1,6 @@
 package com.komangss.submissionjetpack.business.datasource.network
 
-import com.komangss.submissionjetpack.framework.network.model.MovieDetailResponse
 import com.komangss.submissionjetpack.framework.network.model.MovieResultResponse
-import com.komangss.submissionjetpack.framework.network.model.TvShowDetailResponse
 import com.komangss.submissionjetpack.framework.network.model.TvShowResultResponse
 import com.komangss.submissionjetpack.framework.network.services.CatalogServices
 import com.komangss.submissionjetpack.framework.network.utils.ApiResponse
@@ -32,29 +30,4 @@ class CatalogRemoteDataSource private constructor(private val catalogServices: C
         return safeApiCall(Dispatchers.IO) {catalogServices.getTvShows()}
     }
 
-    @ExperimentalCoroutinesApi
-    suspend fun getMovieById(id: Int) : Flow<ApiResponse<MovieDetailResponse>> {
-        return safeApiCall(Dispatchers.IO) {catalogServices.getMovieById(id)}
-    }
-
-    @ExperimentalCoroutinesApi
-    suspend fun getTvShowById(id: Int) : Flow<ApiResponse<TvShowDetailResponse>> {
-        return safeApiCall(Dispatchers.IO) {catalogServices.getTvShowById(id)}
-    }
-
-//    fun getMovieById(id: Int, onMovieReceived: (movieResponse: MovieResponse) -> Unit) {
-//        EspressoIdlingResources.increment()
-//        handler.postDelayed({
-//            jsonHelper.loadMovieById(id)?.let { onMovieReceived(it) }
-//            EspressoIdlingResources.decrement()
-//        }, SERVICE_LATENCY_IN_MILLIS)
-//    }
-//
-//    fun getTvShowById(id: Int, onTvShowReceived: (tvShowResponse: TvShowResponse) -> Unit) {
-//        EspressoIdlingResources.increment()
-//        handler.postDelayed({
-//            jsonHelper.loadTvShowById(id)?.let { onTvShowReceived(it) }
-//            EspressoIdlingResources.decrement()
-//        }, SERVICE_LATENCY_IN_MILLIS)
-//    }
 }
