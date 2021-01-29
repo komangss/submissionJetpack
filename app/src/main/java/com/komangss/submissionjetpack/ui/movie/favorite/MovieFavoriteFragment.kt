d753553c48de616ec6f27c289f4975fd0446a6a8
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-01-29 19:08:24 +0800

    update layout in fragment movie fav

--- app/src/main/java/com/komangss/submissionjetpack/ui/movie/favorite/MovieFavoriteFragment.kt
+++ app/src/main/java/com/komangss/submissionjetpack/ui/movie/favorite/MovieFavoriteFragment.kt
@@ -1,17 +1,17 @@
 package com.komangss.submissionjetpack.ui.movie.favorite
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.komangss.submissionjetpack.R
+import com.komangss.submissionjetpack.viewmodel.ViewModelFactory
+import kotlinx.android.synthetic.main.fragment_movie_favorite.*
 
 class MovieFavoriteFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,5 +19,26 @@ class MovieFavoriteFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_movie_favorite, container, false)
+    }
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        if(activity != null) {
+            val factory = ViewModelFactory.getInstance(requireActivity())
+            val viewModel = ViewModelProvider(this, factory)[MovieFavoriteViewModel::class.java]
+
+            val adapter = MovieFavoriteAdapter()
+
+            viewModel.getFavoriteMovies().observe(viewLifecycleOwner, {
+                adapter.submitList(it)
+            })
+
+            with(fragment_movie_favorite_rv_movie) {
+                layoutManager = LinearLayoutManager(context)
+                setHasFixedSize(true)
+                setAdapter(adapter)
+            }
+        }
     }
 }
