108d96c6b2f4e24bb457e13aff63b59116a9776e
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-01-29 14:53:51 +0800

    add code for section pager adapter

--- app/src/main/java/com/komangss/submissionjetpack/ui/favorite/FavoriteActivity.kt
+++ app/src/main/java/com/komangss/submissionjetpack/ui/favorite/FavoriteActivity.kt
@@ -1,12 +1,19 @@
 package com.komangss.submissionjetpack.ui.favorite
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.komangss.submissionjetpack.R
+import kotlinx.android.synthetic.main.activity_favorite.*
 
 class FavoriteActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favorite)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        view_pager_activity_favorite.adapter = sectionsPagerAdapter
+        tabs_activity_favorite.setupWithViewPager(view_pager_activity_favorite)
+        supportActionBar?.elevation = 0f
+        supportActionBar?.title = "Favorite List"
     }
 }
