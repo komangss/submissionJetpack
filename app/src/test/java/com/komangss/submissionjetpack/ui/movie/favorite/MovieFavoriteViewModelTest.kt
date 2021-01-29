96ab0f326b518b2d732b04320064cf3890beec9f
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-01-29 23:04:17 +0800

    create test for favorite viewModel

--- app/src/test/java/com/komangss/submissionjetpack/ui/movie/favorite/MovieFavoriteViewModelTest.kt
+++ app/src/test/java/com/komangss/submissionjetpack/ui/movie/favorite/MovieFavoriteViewModelTest.kt
@@ -1,3 +1,37 @@
 package com.komangss.submissionjetpack.ui.movie.favorite
 
-class   MovieFavoriteViewModelTest
+import androidx.paging.DataSource
+import androidx.paging.PagedList
+import com.komangss.submissionjetpack.business.domain.model.Movie
+import com.komangss.submissionjetpack.business.repository.CatalogRepository
+import com.komangss.submissionjetpack.framework.cache.model.MovieEntity
+import com.komangss.submissionjetpack.utils.PagedListUtil.mockPagedList
+import com.komangss.submissionjetpack.utils.datagenerator.DomainModelDataGenerator
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.verify
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNotNull
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+
+class  MovieFavoriteViewModelTest {
+    @Test
+    fun getFavoriteMovies() {
+        val movieResults: List<Movie> = DomainModelDataGenerator.generateDummyMovies()
+        val dataSourceFactory: DataSource.Factory<Int, MovieEntity> =
+            mock(DataSource.Factory::class.java) as DataSource.Factory<Int, MovieEntity>
+
+        val repository = com.nhaarman.mockitokotlin2.mock<CatalogRepository> {
+            onBlocking { getFavoriteMovies() } doReturn dataSourceFactory
+        }
+
+        val result: PagedList<Movie> = mockPagedList(movieResults)
+
+        repository.getFavoriteMovies()
+
+        verify(repository).getFavoriteMovies()
+        assertNotNull(result)
+        assertEquals(movieResults.size, result.size)
+    }
+}
--- app/src/test/java/com/komangss/submissionjetpack/ui/tvshow/favorite/TvShowFavoriteViewModelTest.kt
+++ app/src/test/java/com/komangss/submissionjetpack/ui/tvshow/favorite/TvShowFavoriteViewModelTest.kt
@@ -1,3 +1,37 @@
 package com.komangss.submissionjetpack.ui.tvshow.favorite
 
-class TvShowFavoriteViewModelTest
+import androidx.paging.DataSource
+import androidx.paging.PagedList
+import com.komangss.submissionjetpack.business.domain.model.TvShow
+import com.komangss.submissionjetpack.business.repository.CatalogRepository
+import com.komangss.submissionjetpack.framework.cache.model.TvShowEntity
+import com.komangss.submissionjetpack.utils.PagedListUtil.mockPagedList
+import com.komangss.submissionjetpack.utils.datagenerator.DomainModelDataGenerator
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.verify
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNotNull
+import org.junit.Test
+import org.mockito.Mockito.mock
+
+
+class TvShowFavoriteViewModelTest {
+    @Test
+    fun getFavoriteTvShows() {
+        val tvShowResults: List<TvShow> = DomainModelDataGenerator.generateDummyTvShows()
+        val dataSourceFactory: DataSource.Factory<Int, TvShowEntity> =
+            mock(DataSource.Factory::class.java) as DataSource.Factory<Int, TvShowEntity>
+
+        val repository = com.nhaarman.mockitokotlin2.mock<CatalogRepository> {
+            onBlocking { getFavoriteTvShows() } doReturn dataSourceFactory
+        }
+
+        val result: PagedList<TvShow> = mockPagedList(tvShowResults)
+
+        repository.getFavoriteTvShows()
+
+        verify(repository).getFavoriteTvShows()
+        assertNotNull(result)
+        assertEquals(tvShowResults.size, result.size)
+    }
+}
