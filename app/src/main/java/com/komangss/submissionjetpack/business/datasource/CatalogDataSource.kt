366e7c794a56815d55bd694d51020e0a8c9fea2e
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-01-29 15:32:38 +0800

    fix error remove get by id

--- app/src/main/java/com/komangss/submissionjetpack/business/datasource/CatalogDataSource.kt
+++ app/src/main/java/com/komangss/submissionjetpack/business/datasource/CatalogDataSource.kt
@@ -3,6 +3,7 @@ package com.komangss.submissionjetpack.business.datasource
 import com.komangss.submissionjetpack.business.domain.model.Movie
 import com.komangss.submissionjetpack.business.domain.model.TvShow
 import com.komangss.submissionjetpack.vo.Resource
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 
 interface CatalogDataSource {
@@ -13,4 +14,10 @@ interface CatalogDataSource {
     suspend fun setTvShowFavorite(tvShow: TvShow)
 
     suspend fun setMovieFavorite(movie: Movie)
+
+    @ExperimentalCoroutinesApi
+    suspend fun getMovieById(id: Int): Flow<Resource<Movie>>
+
+    @ExperimentalCoroutinesApi
+    suspend fun getTvShowById(id: Int): Flow<Resource<TvShow>>
 }
