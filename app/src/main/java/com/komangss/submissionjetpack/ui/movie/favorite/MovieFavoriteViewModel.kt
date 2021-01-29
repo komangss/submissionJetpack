7b67d027ecdc1c05f98bbf2890904a97fbe7753b
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-01-29 16:14:17 +0800

    add get favorite in viewmodel

--- app/src/main/java/com/komangss/submissionjetpack/ui/movie/favorite/MovieFavoriteViewModel.kt
+++ app/src/main/java/com/komangss/submissionjetpack/ui/movie/favorite/MovieFavoriteViewModel.kt
@@ -1,6 +1,13 @@
 package com.komangss.submissionjetpack.ui.movie.favorite
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.komangss.submissionjetpack.business.domain.model.Movie
 import com.komangss.submissionjetpack.business.repository.CatalogRepository
 
-class MovieFavoriteViewModel(catalogRepository: CatalogRepository) : ViewModel()
+class MovieFavoriteViewModel(private val catalogRepository: CatalogRepository) : ViewModel() {
+    suspend fun getFavoriteMovies() : LiveData<PagedList<Movie>> =
+        LivePagedListBuilder(catalogRepository.getFavoriteMovies(), 5).build()
+}
--- app/src/main/java/com/komangss/submissionjetpack/ui/tvshow/favorite/TvShowFavoriteViewModel.kt
+++ app/src/main/java/com/komangss/submissionjetpack/ui/tvshow/favorite/TvShowFavoriteViewModel.kt
@@ -1,6 +1,13 @@
 package com.komangss.submissionjetpack.ui.tvshow.favorite
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.komangss.submissionjetpack.business.domain.model.TvShow
 import com.komangss.submissionjetpack.business.repository.CatalogRepository
 
-class TvShowFavoriteViewModel(catalogRepository: CatalogRepository) : ViewModel()
+class TvShowFavoriteViewModel(private val catalogRepository: CatalogRepository) : ViewModel() {
+    suspend fun getFavoriteTvShows() : LiveData<PagedList<TvShow>> =
+        LivePagedListBuilder(catalogRepository.getFavoriteTvShows(), 5).build()
+}
