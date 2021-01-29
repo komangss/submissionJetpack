3a679d54b74da09f77ec525e0b994e85d023a6a6
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-01-29 21:32:28 +0800

    create favorite activity ui test

--- app/src/androidTest/java/com/komangss/submissionjetpack/ui/favorite/FavoriteActivityTest.kt
+++ app/src/androidTest/java/com/komangss/submissionjetpack/ui/favorite/FavoriteActivityTest.kt
@@ -1,3 +1,26 @@
 package com.komangss.submissionjetpack.ui.favorite
 
-class FavoriteActivityTest
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import com.komangss.submissionjetpack.R
+import com.komangss.submissionjetpack.ui.rule.lazyActivityScenarioRule
+import org.junit.Rule
+import org.junit.Test
+
+class FavoriteActivityTest {
+
+    @get:Rule
+    val rule = lazyActivityScenarioRule<FavoriteActivity>(launchActivity = true)
+
+    @Test
+    fun swipePage() {
+        Espresso.onView(ViewMatchers.withId(R.id.view_pager_activity_favorite))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.view_pager_activity_favorite))
+            .perform(ViewActions.swipeLeft())
+        Espresso.onView(ViewMatchers.withId(R.id.view_pager_activity_favorite))
+            .perform(ViewActions.swipeRight())
+    }
+}
