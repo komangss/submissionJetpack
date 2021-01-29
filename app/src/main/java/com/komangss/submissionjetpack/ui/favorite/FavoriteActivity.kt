5ed3c8811d21c7a8b9dc924a86bb68ab46e19dd3
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-01-29 14:42:49 +0800

    create favorite activity

---
+++ app/src/main/java/com/komangss/submissionjetpack/ui/favorite/FavoriteActivity.kt
@@ -0,0 +1,12 @@
+package com.komangss.submissionjetpack.ui.favorite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.komangss.submissionjetpack.R
+
+class FavoriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+    }
+}
