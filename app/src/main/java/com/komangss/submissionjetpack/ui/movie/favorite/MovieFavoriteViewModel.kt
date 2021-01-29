9b1e77deebe8a8d2e68ae3604c17b1edbbf8d3c3
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-01-29 15:17:45 +0800

    create view model for favorite

---
+++ app/src/main/java/com/komangss/submissionjetpack/ui/movie/favorite/MovieFavoriteViewModel.kt
@@ -0,0 +1,5 @@
+package com.komangss.submissionjetpack.ui.movie.favorite
+
+import androidx.lifecycle.ViewModel
+
+class MovieFavoriteViewModel : ViewModel()
---
+++ app/src/main/java/com/komangss/submissionjetpack/ui/tvshow/favorite/TvShowFavoriteViewModel.kt
@@ -0,0 +1,5 @@
+package com.komangss.submissionjetpack.ui.tvshow.favorite
+
+import androidx.lifecycle.ViewModel
+
+class TvShowFavoriteViewModel : ViewModel()
