9139d856615fbae32cf2e620c259d1fcf0e1726e
komangss <81404039+Axlsurya02@users.noreply.github.com>
2021-05-26 22:16:15 +0800

    fix wrong variable name in mapper test and fix assertion genre id mapper test

--- app/src/test/java/com/komangss/submissionjetpack/framework/mapper/MapperTest.kt
+++ app/src/test/java/com/komangss/submissionjetpack/framework/mapper/MapperTest.kt
@@ -9,23 +9,25 @@ class MapperTest {
     @Test
     fun `test convert example id from entity into domain model using regex`() {
         val genreIdsToTest = "[16, 14, 12, 35, 10751]"
+        val expectedResult = listOf(16, 14, 12, 35, 10751)
+
         val m = Pattern.compile("\\d+").matcher(genreIdsToTest)
         val result: ArrayList<Int> = ArrayList()
         while (m.find()) {
             result.add(m.group().toInt())
         }
-        assert(result.size > 0)
+        Assert.assertEquals(expectedResult, result)
     }
 
     @Test
     fun `test convert example id from entity into domain model using kotlin function`() {
-        var genreIdsToTest = "[16, 14, 12, 35, 10751]"
-        genreIdsToTest = genreIdsToTest.replace(" ", "")
-        genreIdsToTest = genreIdsToTest.substring(1, genreIdsToTest.length - 1)
-
+        var originalCountries = "[16, 14, 12, 35, 10751]"
         val expectedResult = listOf(16, 14, 12, 35, 10751)
 
-        val result: List<Int> = genreIdsToTest.split(",").map {
+        originalCountries = originalCountries.replace(" ", "")
+        originalCountries = originalCountries.substring(1, originalCountries.length - 1)
+
+        val result: List<Int> = originalCountries.split(",").map {
             it.toInt()
         }
 
@@ -34,13 +36,13 @@ class MapperTest {
 
     @Test
     fun `test convert example original country from entity into domain model`() {
-        var genreIdsToTest = "[en, uk, id]"
-        genreIdsToTest = genreIdsToTest.replace(" ", "")
-        genreIdsToTest = genreIdsToTest.substring(1, genreIdsToTest.length - 1)
-
+        var originalCountries = "[en, uk, id]"
         val expectedResult = listOf("en", "uk", "id")
 
-        val result: List<String> = genreIdsToTest.split(",")
+        originalCountries = originalCountries.replace(" ", "")
+        originalCountries = originalCountries.substring(1, originalCountries.length - 1)
+
+        val result: List<String> = originalCountries.split(",")
         Assert.assertEquals(expectedResult, result)
     }
 }
