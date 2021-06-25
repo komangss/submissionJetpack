313adf3e85c161197e7d360a8ffc78d30ad5beb5
Antonio Komang Yudistira <komanggt12@gmail.com>
2021-06-26 00:07:48 +0800

    add favorite activity binding

--- app/src/main/java/com/komangss/submissionjetpack/ui/favorite/FavoriteActivity.kt
+++ app/src/main/java/com/komangss/submissionjetpack/ui/favorite/FavoriteActivity.kt
@@ -3,20 +3,24 @@ package com.komangss.submissionjetpack.ui.favorite
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.komangss.submissionjetpack.R
+import com.komangss.submissionjetpack.databinding.ActivityFavoriteBinding
 import com.komangss.submissionjetpack.ui.home.HomeActivity
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.activity_favorite.*
 
 @AndroidEntryPoint
 class FavoriteActivity : AppCompatActivity() {
+
+    private var _binding: ActivityFavoriteBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_favorite)
+        _binding = ActivityFavoriteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
-        view_pager_activity_favorite.adapter = sectionsPagerAdapter
-        tabs_activity_favorite.setupWithViewPager(view_pager_activity_favorite)
+        binding.viewPagerActivityFavorite.adapter = sectionsPagerAdapter
+        binding.tabsActivityFavorite.setupWithViewPager(binding.viewPagerActivityFavorite)
         supportActionBar?.elevation = 0f
         supportActionBar?.title = "Favorite List"
     }
